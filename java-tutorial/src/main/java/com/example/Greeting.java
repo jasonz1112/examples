d9376cfc20c3d1330f83f6fe647b2dcee8645f2d
jasonz1112 <jasonz1112@gmail.com>
2021-04-27 12:04:26 -0700

    damn it

--- java-tutorial/src/main/java/com/example/Greeting.java
+++ java-tutorial/src/main/java/com/example/Greeting.java
@@ -3,6 +3,7 @@ package com.example;
 public class Greeting {
     public static void sayHi() {
             System.out.println("Hi!");
+            System.out.println("Bazel is very stupid");
     }
 }
 
